d966f336d3d1752b5a6c8ab5488569248d7f3e36
Weiqiang Yu <w8yu@uwaterloo.ca>
2023-03-18 00:22:44 -0400

    update_client

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 const schema = require("./schema/schema");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const connectDB = require("./config/db");
 const {graphqlHTTP} = require("express-graphql");
 const app = express();
 
 connectDB();
-
+app.use(cors());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development"
